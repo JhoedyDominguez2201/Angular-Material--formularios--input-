a80342a339b3f794af3fcccec89b1efc494274b4
JhoedyDominguez2201 <l17390333@chetumal.tecnm.mx>
2022-06-26 19:05:08 -0500

    Commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
